07136ba290054a5838f056cea831a0ef0006d7dc
yaozeyuan <yaozeyuan001@lianjia.com>
2020-03-27 02:53:31 +0800

    feat: 提交前端模板代码, 准备进行实际测试

--- index.ts
+++ index.ts
@@ -2,19 +2,17 @@
 // 1.   将template目录中的模板文件, 释放到当前所在文件夹中
 import path from "path";
 import shelljs from "shelljs";
-export function unpackage() {
+export default function unpackage() {
   // 运行所在目录(将template目录中文件释放到当前目录下)
   let runtimeAtUri = process.cwd();
   // 当前文件所在目录
   let fileUri = __dirname;
 
-  let templateUri = path.resolve(fileUri, "template");
+  let templateUri = path.resolve(fileUri, "template") + path.sep + "*";
 
   console.log("fileUri => ", fileUri);
-  console.log("runtimeAtUri => ", runtimeAtUri);
   console.log("templateUri => ", templateUri);
-  // shelljs.cp("-rf", templateUri, runtimeAtUri);
+  console.log("runtimeAtUri => ", runtimeAtUri);
+  shelljs.cp("-rf", templateUri, runtimeAtUri);
   console.log("success");
 }
-
-// unpackage();

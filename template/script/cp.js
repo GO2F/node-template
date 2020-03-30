16b9e57f72ba21e5c62bd82656118f49d93582e5
yaozeyuan <yaozeyuan001@lianjia.com>
2020-03-31 01:40:47 +0800

    feat: 在cp脚本中完成对静态资源文件夹的清理

--- template/script/cp.js
+++ template/script/cp.js
@@ -3,9 +3,13 @@ const path = require("path");
 const shelljs = require("shelljs")
 
 
-let distPath = path.resolve("./dist", "*")
-let staticPath = path.resolve("../static")
+let distPath = path.resolve(__dirname, "..", "dist", "*")
+let staticPath = path.resolve(__dirname, "..", "..", "static")
+let staticSubPath = path.resolve(__dirname, "..", "..", "static", "*")
 console.log("静态资源编译完毕, 准备进行迁移")
+console.log(`清空静态文件夹${staticSubPath}下所有文件`)
+shelljs.rm("-rf", staticSubPath)
+console.log(`静态资源文件夹清理完毕`)
 console.log(`开始将编译后资源由${distPath}复制到${staticPath}文件夹`)
 shelljs.cp("-rf", distPath, staticPath)
 console.log(`静态资源迁移完毕`)

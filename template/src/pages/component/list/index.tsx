7ffd5899f493f45bce5b3a4eef561497338dde91
yaozeyuan <yaozeyuan001@lianjia.com>
2020-03-31 11:36:39 +0800

    fix: 获取unique_key对应字段, 默认为id

--- template/src/pages/component/list/index.tsx
+++ template/src/pages/component/list/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Card, message, Button, Modal } from 'antd';
-import Link from 'umi/link';
-import { Divider } from 'antd';
+import React from "react";
+import { PageHeaderWrapper } from "@ant-design/pro-layout";
+import { Card, message, Button, Modal } from "antd";
+import Link from "umi/link";
+import { Divider } from "antd";
 
-import request from 'umi-request';
-import { TypeExtends } from '../type_extend';
-import TableBase from './table_basic';
+import request from "umi-request";
+import { TypeExtends } from "../type_extend";
+import TableBase from "./table_basic";
 // import styles from './index.less';
 
 export default class TablePage extends React.Component<any, any> {
   state = {
-    itemList: [],
+    itemList: []
   };
+
+  // 获取unique_key对应字段, 默认为id
+  getUniqueKey() {
+    let extendConfig: TypeExtends = this.props.route.extendConfig;
+
+    let keyConfigList = extendConfig.keyList;
+    for (let keyConfig of keyConfigList) {
+      if (keyConfig.is_unique_key) {
+        return keyConfig.key;
+      }
+    }
+    return "id";
+  }
 
   async asyncFetchData() {
     const extendConfig: TypeExtends = this.props.route.extendConfig;
@@ -23,7 +36,7 @@ export default class TablePage extends React.Component<any, any> {
     });
     let itemList = response?.data || [];
     this.setState({
-      itemList: itemList,
+      itemList: itemList
     });
   }
 
@@ -34,14 +47,14 @@ export default class TablePage extends React.Component<any, any> {
     let response = await request
       .post(api, {
         params: {
-          id: id,
-        },
+          id: id
+        }
       })
       .catch(() => {
         return {};
       });
     if (response.code === 0) {
-      message.info('删除成功', 1);
+      message.info("删除成功", 1);
     }
     // 删除完毕后重新载入数据
     await this.asyncFetchData();
@@ -50,7 +63,7 @@ export default class TablePage extends React.Component<any, any> {
   componentDidMount() {
     // 初始化数据
     this.asyncFetchData();
-    console.log('数据初始化完毕 this.state.itemList => ', this.state.itemList);
+    console.log("数据初始化完毕 this.state.itemList => ", this.state.itemList);
   }
 
   render(): React.ReactNode {
@@ -69,35 +82,36 @@ export default class TablePage extends React.Component<any, any> {
       let columnItem = {
         title: keyConfig.title,
         dataIndex: keyConfig.key,
-        key: keyConfig.key,
+        key: keyConfig.key
       };
       tableColumnList.push(columnItem);
     }
+    let uniqueKey = this.getUniqueKey();
     // 添加操作栏
     tableColumnList.push({
-      title: '操作',
-      key: 'action',
-      fixed: 'right',
+      title: "操作",
+      key: "action",
+      fixed: "right",
       render: (text: string, record: any) => {
         return (
           <div>
             <span>
               {extendConfig?.pageConfig?.detail ? (
-                <Link to={`/component/detail/${record.id}`}>详情</Link>
+                <Link to={`/component/detail/${record[uniqueKey]}`}>详情</Link>
               ) : null}
               <span>&nbsp;</span>
               {extendConfig?.pageConfig?.update ? (
-                <Link to={`/component/update/${record.id}`}>修改</Link>
+                <Link to={`/component/update/${record[uniqueKey]}`}>修改</Link>
               ) : null}
               <Divider type="vertical" />
               <Button
                 type="link"
                 onClick={() => {
                   Modal.confirm({
-                    content: `确认删除记录${record.id}?`,
+                    content: `确认删除记录${record[uniqueKey]}?`,
                     onOk: () => {
-                      this.asyncRemoveItem(record.id);
-                    },
+                      this.asyncRemoveItem(record[uniqueKey]);
+                    }
                   });
                 }}
               >
@@ -106,7 +120,7 @@ export default class TablePage extends React.Component<any, any> {
             </span>
           </div>
         );
-      },
+      }
     });
 
     return (

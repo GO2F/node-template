6735634c53c925f4e07225d2347626c820bde49f
yaozeyuan <yaozeyuan001@lianjia.com>
2020-04-02 02:25:43 +0800

    feat: 更新测试配置

--- template/config/go2fe_generate_config.js
+++ template/config/go2fe_generate_config.js
@@ -1,58 +1,56 @@
 export default [
   {
-    "version": 0.1,
-    "name":"demo-project",
-    "data_model": {
-      "key_list": [
+    version: 0.1,
+    data_model: {
+      key_list: [
+        { key: 'ID', title: 'id', var_type: 'string', is_show_in_list: true, is_unique_key: true },
         {
-          "key": "id",
-          title:"id",
-          "var_type": "string",
-          "is_show_in_list": true,
-          "is_unique_key": true
+          key: 'DisplayName',
+          title: '组件名',
+          var_type: 'string',
+          is_show_in_list: true,
+          is_unique_key: false,
         },
         {
-          "key": "display_name",
-          title:"组件名",
-          "var_type": "string",
-          "is_show_in_list": true,
-          "is_unique_key": false
+          key: 'PackageName',
+          title: '包名',
+          var_type: 'string',
+          is_show_in_list: true,
+          is_unique_key: false,
         },
         {
-          "key": "package_name",
-          title:"npm包名",
-          "var_type": "string",
-          "is_show_in_list": false,
-          "is_unique_key": false
+          key: 'DevListJSON',
+          title: '开发者',
+          var_type: 'string',
+          is_show_in_list: true,
+          is_unique_key: false,
         },
         {
-          "key": "dev_list_json",
-          "var_type": "string",
-          "is_show_in_list": false,
-          "is_unique_key": false
+          key: 'Description',
+          title: '描述',
+          var_type: 'string',
+          is_show_in_list: true,
+          is_unique_key: false,
         },
         {
-          "key": "description",
-          "var_type": "string",
-          "is_show_in_list": false,
-          "is_unique_key": false
+          key: 'SiteURL',
+          title: '网站主页',
+          var_type: 'string',
+          is_show_in_list: true,
+          is_unique_key: false,
         },
         {
-          "key": "site_url",
-          "var_type": "string",
-          "is_show_in_list": false,
-          "is_unique_key": false
+          key: 'Remark',
+          title: '备注',
+          var_type: 'string',
+          is_show_in_list: false,
+          is_unique_key: false,
         },
-        {
-          "key": "remark",
-          "var_type": "string",
-          "is_show_in_list": false,
-          "is_unique_key": false
-        }
-      ]
+      ],
     },
-    "page_config": { "create": true, "update": true, "detail": true },
-    "base_url_path": "/component",
-    "base_api_path": "/api/component"
-  }
-]
+    page_config: { create: true, update: true, detail: true },
+    name: '测试模型',
+    base_url_path: '/component',
+    base_api_path: '/api/component',
+  },
+];
